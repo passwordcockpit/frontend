1a9c5ac005dd3afc96e6e5fcf83f666e6aec088a
Paride Buetti <paride.buetti@blackpoints.ch>
2019-01-15 17:38:50 +0100

    Removed integrity check form index.html

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,77 +3,80 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    outputPaths: {
-      app: {
-        css: {
-          'styles': '/assets/styles.css'
+    let app = new EmberApp(defaults, {
+        SRI: {
+            enabled: false,
+        },
+        outputPaths: {
+            app: {
+                css: {
+                    'styles': '/assets/styles.css'
+                }
+            }
         }
-      }
-    }
-  });
-  // Jquery form validation
-  app.import('node_modules/jquery-form-validator/form-validator/jquery.form-validator.min.js');
+    });
+    // Jquery form validation
+    app.import('node_modules/jquery-form-validator/form-validator/jquery.form-validator.min.js');
 
-  // Import Bootstrap JS
-  app.import('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js');
+    // Import Bootstrap JS
+    app.import('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js');
 
-  // Import FontAwesome icons
-  /*
-  app.import('node_modules/font-awesome/fonts/fontawesome-webfont.eot',{ destDir: 'fonts' }); 
-  app.import('node_modules/font-awesome/fonts/fontawesome-webfont.svg',{ destDir: 'fonts' }); 
-  app.import('node_modules/font-awesome/fonts/fontawesome-webfont.ttf',{ destDir: 'fonts' }); 
-  app.import('node_modules/font-awesome/fonts/fontawesome-webfont.woff',{ destDir: 'fonts' }); 
-  app.import('node_modules/font-awesome/fonts/fontawesome-webfont.woff2',{ destDir: 'fonts' });
-  */
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.eot',{ destDir: 'fonts' });
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.svg',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2',{ destDir: 'fonts' });
+    // Import FontAwesome icons
+    /*
+    app.import('node_modules/font-awesome/fonts/fontawesome-webfont.eot',{ destDir: 'fonts' }); 
+    app.import('node_modules/font-awesome/fonts/fontawesome-webfont.svg',{ destDir: 'fonts' }); 
+    app.import('node_modules/font-awesome/fonts/fontawesome-webfont.ttf',{ destDir: 'fonts' }); 
+    app.import('node_modules/font-awesome/fonts/fontawesome-webfont.woff',{ destDir: 'fonts' }); 
+    app.import('node_modules/font-awesome/fonts/fontawesome-webfont.woff2',{ destDir: 'fonts' });
+    */
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.eot', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.svg', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2', { destDir: 'fonts' });
 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.eot',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.svg',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2',{ destDir: 'fonts' }); 
- 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff',{ destDir: 'fonts' }); 
- app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2',{ destDir: 'fonts' }); 
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.eot', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.svg', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2', { destDir: 'fonts' });
+
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff', { destDir: 'fonts' });
+    app.import('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2', { destDir: 'fonts' });
 
 
-  // Import jQuery growl notifications
-  app.import('node_modules/jquery.growl/javascripts/jquery.growl.js');
+    // Import jQuery growl notifications
+    app.import('node_modules/jquery.growl/javascripts/jquery.growl.js');
 
-  // Import Fetch
-  //app.import('node_modules/whatwg-fetch/fetch.js');
+    // Import Fetch
+    //app.import('node_modules/whatwg-fetch/fetch.js');
 
-  // Import File-saver
-  app.import('node_modules/file-saver/FileSaver.min.js');
+    // Import File-saver
+    app.import('node_modules/file-saver/FileSaver.min.js');
 
-  // Import password generation
-  app.import('vendor/pGenerator.js');
+    // Import password generation
+    app.import('vendor/pGenerator.js');
 
-  // Import password encrypt
-  app.import('vendor/sjcl.js');
+    // Import password encrypt
+    app.import('vendor/sjcl.js');
 
-  app.import('vendor/script.js');
+    app.import('vendor/script.js');
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+    // Use `app.import` to add additional libraries to the generated
+    // output files.
+    //
+    // If you need to use different assets in different
+    // environments, specify an object as the first parameter. That
+    // object's keys should be the environment name and the values
+    // should be the asset to use in that environment.
+    //
+    // If the library that you are including contains AMD or ES6
+    // modules that you would like to import into your application
+    // please specify an object with the list of modules as keys
+    // along with the exports of each module as its value.
 
-  return app.toTree();
+    return app.toTree();
 };
