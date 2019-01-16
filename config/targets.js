341ec523be3a5268af3afb6502d3172e9b9fe904
Nhat Nguyen <nhat.nguyen@blackpoints.ch>
2019-01-16 09:25:50 +0100

    Refs#14916: Modified Licence github links

--- config/targets.js
+++ config/targets.js
@@ -1,7 +1,7 @@
 /** 
-* @see https://github.com/password-cockpit/frontend for the canonical source repository 
+* @see https://github.com/passwordcockpit/frontend for the canonical source repository 
 * @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
-* @license https://github.com/password-cockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
+* @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
 */
 
 module.exports = {
