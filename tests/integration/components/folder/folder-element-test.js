9c687ef575459cef533deed45c279d08146b8d3e
Nhat Nguyen <nhat.nguyen@blackpoints.ch>
2019-01-16 09:29:47 +0100

    Refs#14916: Added licenses

--- tests/integration/components/folder/folder-element-test.js
+++ tests/integration/components/folder/folder-element-test.js
@@ -1,3 +1,8 @@
+/** 
+* @see https://github.com/passwordcockpit/frontend for the canonical source repository 
+* @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
+* @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
+*/
 
 import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
--- tests/unit/models/permission-test.js
+++ tests/unit/models/permission-test.js
@@ -1,3 +1,9 @@
+/** 
+* @see https://github.com/passwordcockpit/frontend for the canonical source repository 
+* @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
+* @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
+*/
+
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('permission', 'Unit | Model | permission', {
