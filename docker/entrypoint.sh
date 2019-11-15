94cfa7541106c8000699a41c7afa0af7ccb87837
Paride Buetti <paride.buetti@blackpoints.ch>
2019-11-15 09:28:36 +0100

    Modified Docker files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 
 # check volume
-count=$(ls -f | wc -l)
+#count=$(ls -f | wc -l)
 
 # remove .DS_Store if is the only file
-if [ $count -eq "3" ] && [ -a .DS_Store ]; then
-    rm -rf .DS_Store
-    echo >&2 ".DS_Store removed from $PWD"
-    count=$((count - 1))
-fi
+#if [ $count -eq "3" ] && [ -a .DS_Store ]; then
+#    rm -rf .DS_Store
+#    echo >&2 ".DS_Store removed from $PWD"
+#    count=$((count - 1))
+#fi
 
 # install passwordcockpit only if the volume is empty
-if  [ $count -gt "2" ]; then
-        echo >&2 "WARNING: $PWD is not empty! Passwordcockpit will not be installed"
-
-        if [ "${PASSWORDCOCKPIT_FRONTEND_DEVELOPMENTMODE_FOR_ENTRYPOINT}" -eq "1" ]; then
-            # develop mode
-            # npm install
-            npm prune
-            npm install
-            npm install -g ember-cli
-            echo >&2 "npm install"
-        fi
-else
+#if  [ $count -gt "2" ]; then
+#        echo >&2 "WARNING: $PWD is not empty! Passwordcockpit will not be installed"
+#else
     # move passwordcockpit source in container
-    shopt -s dotglob
-    mv /usr/src/passwordcockpit/* ./
-    echo >&2 "Source moved in $PWD"
+    sed -ri -e 's!PASSWORDCOCKPIT_FRONTEND_BASEHOST_TOKEN!'${PASSWORDCOCKPIT_BACKEND_BASEHOST}'!g' config/local.js  
+    echo >&2 "local.js updated"
 
-    if [ "${PASSWORDCOCKPIT_FRONTEND_DEVELOPMENTMODE_FOR_ENTRYPOINT}" -eq "1" ]; then
-        # develop mode
-        # npm install
-        npm install
-        npm install -g ember-cli
-
-        sed -ri -e 's!PASSWORDCOCKPIT_FRONTEND_BASEHOST_TOKEN!'${PASSWORDCOCKPIT_FRONTEND_BASEHOST}'!g' config/local.js
-        echo >&2 "local.js updated"
-    else
-        # production mode
-        sed -ri -e 's!PASSWORDCOCKPIT_FRONTEND_BASEHOST_TOKEN!'${PASSWORDCOCKPIT_FRONTEND_BASEHOST}'!g' index.html
-        sed -ri -e 's!PASSWORDCOCKPIT_FRONTEND_BASEHOST_TOKEN!'${PASSWORDCOCKPIT_FRONTEND_BASEHOST}'!g' assets/*.*
-        echo >&2 "files updated"
-    fi
-fi
+#fi
 
 exec "$@"
