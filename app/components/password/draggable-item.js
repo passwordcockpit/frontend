a50eb74dda0e1c1c6b64970817dc80cf2c4f61c9
Dominic Detta <dominic.detta@blackpoints.ch>
2019-11-21 09:52:01 +0100

    Add license jsdocs

--- app/components/password/draggable-item.js
+++ app/components/password/draggable-item.js
@@ -1,3 +1,9 @@
+/** 
+* @see https://github.com/passwordcockpit/frontend for the canonical source repository 
+* @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
+* @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
+*/
+
 import Component from '@ember/component';
 
 export default Component.extend({
