d16d032410edb553116685324f4fa50b76d17cf9
Alex Negri <alex.negri@blackpoints.ch>
2022-02-25 15:43:48 +0100

    Removed unused injections

--- app/routes/sorry-page.js
+++ app/routes/sorry-page.js
@@ -5,7 +5,6 @@
 */
 
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
 
 export default Route.extend({
     beforeModel() {
--- app/routes/users/new-user.js
+++ app/routes/users/new-user.js
@@ -6,7 +6,6 @@
 
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
     beforeModel() {
