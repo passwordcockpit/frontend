493a9d1fdb4346487efab8d748495a70593f44c4
Alex Negri <alex.negri@blackpoints.ch>
2022-03-01 19:27:58 +0100

    Updated EmberJS to 3.20, fixed conflicts and installed suggested npm package versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,9 +23,7 @@ module.exports = {
   env: {
     browser: true
   },
-  rules: {
-    "ember/jquery-ember-run": 0
-  },
+  rules: {},
   overrides: [
     // node files
     {
@@ -45,13 +43,12 @@ module.exports = {
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-
+      extends: ['plugin:node/recommended'],
+      rules: {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
         'node/no-unpublished-require': 'off'
-      })
+      }
     },
 
     // test files
--- config/targets.js
+++ config/targets.js
@@ -3,6 +3,20 @@
 * @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
 * @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
 */
+'use strict';
+
+const browsers = [
+  'last 1 Chrome versions',
+  'last 1 Firefox versions',
+  'last 1 Safari versions'
+];
+
+const isCI = Boolean(process.env.CI);
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 11');
+}
 
 module.exports = {
   browsers: [
