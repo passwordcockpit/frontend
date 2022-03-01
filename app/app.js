f6c9e243596110458a9ef3f33b84cdda9022c6e1
Alex Negri <alex.negri@blackpoints.ch>
2022-03-01 14:53:57 +0100

    Updated to 3.14, solved conflicts and installed new npm recommended packages versions

--- app/app.js
+++ app/app.js
@@ -10,15 +10,13 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import TextField from '@ember/component/text-field';
 
-const App = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
 loadInitializers(App, config.modulePrefix);
-
-export default App;
 
 TextField.reopen({
     attributeBindings: ['data-validation', 'data-validation-length']
