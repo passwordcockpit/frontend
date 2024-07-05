b6bfd49be513575b528c5691deab38a1e7fb2163
Saeverin <hsd.ch@bluewin.ch>
2024-07-05 11:54:37 +0200

    fix(#91): added dependencies for testing and regenerated package-lock.json, fixed some deprecation-warnings and changed optional-features

---
+++ app/services/store.js
@@ -0,0 +1,7 @@
+/** 
+* @see https://github.com/passwordcockpit/frontend for the canonical source repository 
+* @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch) 
+* @license https://github.com/passwordcockpit/frontend/blob/master/LICENSE.md BSD 3-Clause License 
+*/
+
+export { default } from 'ember-data/store';
