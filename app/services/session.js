160b5c9d013e519729c4936ae7e8c1e879d69000
Carolin Maeder <carolin.maeder@blackpoints.ch>
2026-02-19 17:27:34 +0100

    chore(#115): update packages, ember-simple-auth and ember-ajax

---
+++ app/services/session.js
@@ -0,0 +1,6 @@
+// app/services/session.js
+import SessionService from 'ember-simple-auth/services/session';
+
+export default class Session extends SessionService {
+  // keep any custom additions here
+}
